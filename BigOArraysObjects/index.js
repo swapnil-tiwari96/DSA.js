d6399df0ccbfc3fc0a2779bfae9bfff97c2aa49c
Swapnil Tiwari <swaptiwari@outlook.com>
2022-08-04 07:29:19 +0530

    Arrays and Objects Big O

---
+++ BigOArraysObjects/index.js
@@ -0,0 +1,29 @@
+// Big O of Objects 
+
+// Insertion - O(1)
+// Removal - O(1)
+// Searching - O(n)
+// Access - O(1)
+// Object.keys() - O(n) (returns an array with all the keys)
+// Object.values() - O(n) (returns an array with all the values)
+// Object.entries() - O(n)
+// hasOwnProperty() - O(1) (checks if the object has that property or not)
+
+
+// Big O of Arrays
+
+// Insertion at beginning - O(n)
+// Insertion at end - O(1)
+// Removal at beginning - O(n)
+// Removal at end - O(1)
+// Searching - O(n)
+// Access - O(1)
+// push() - O(1)
+// pop() - O(1)
+// shift - O(n)
+// unshift - O(n)
+// concat - O(n)
+// slice - O(n)
+// splice - O(n)
+// sort - O(n * logn)
+// forEach/map/filter/reduce - O(n)
