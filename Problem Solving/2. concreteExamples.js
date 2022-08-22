86a8579830c5d18cbdb91b8d43c5c445444fbc83
Swapnil Tiwari <swaptiwari@outlook.com>
2022-08-22 23:59:48 +0530

    add

---
+++ Problem Solving/2. concreteExamples.js
@@ -0,0 +1,19 @@
+// Explore examples - coming up with examples help you to undestand the problem better. (User Stories, Unit Tests)
+
+// 1. Start with simple examples
+// 2. Progress to complex examples
+// 3. Explore examples with empty inputs
+// 4. Explore with wront inputs
+
+// Problem - Write a function that takes an input and returns counts of each character in a string. 
+// 1. Normal 
+"abaaca"
+// From this I can think should it have all characters or just the characters which appear. 
+
+// 2. if it has upper and lower case. Should I think about spaces? Should it have numbers or special characters? 
+"Hello hi"
+
+// 3. How to handle empty input?
+
+// 4. What if there is no string nut a number? 
+12345
--- Problem Solving/concreteExamples.js
+++
@@ -1,1 +0,0 @@
-// Explore examples - coming up with examples help you to undestand the problem better. (User Stories, Unit Tests)
